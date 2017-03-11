5112b66797400fa4b1d039e5c3a7c1cad2afd884
sgordon834 <sgordon834@gmail.com>
2017-03-11 11:17:47 -0800

    ready merge

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var clientArray = [];
 
-function Client(name, email, jobDescription, location){
+function Client(name, contact, descript, loc){
   this.name = name;
   this.email = email;
   this.descript = descript;
@@ -8,23 +8,23 @@ function Client(name, email, jobDescription, location){
   clientArray.push(this);
 }
 
-function handleForm(e){
-  e.preventDefault();
-  console.log(e);
-
-  var name = e.target.name.value;
-  var email = e.target.email.value;
-  var descript = e.target.descript.value;
-  var loc = e.target.loc.value;
-
-  console.log(name, loc);
-
-  var newClient = new Client(name, email, descript, loc);
-
-  e.target.name.value = null;
-  e.target.email.value = null;
-  e.target.descript.value = null;
-  e.target.loc.value = null;
+// function handleForm(e){
+//   e.preventDefault();
+//   console.log(e);
+//
+//   var name = e.target.name.value;
+//   var email = e.target.email.value;
+//   var descript = e.target.descript.value;
+//   var loc = e.target.loc.value;
+//
+//   console.log(name, loc);
+//
+//   var newClient = new Client(name, email, descript, loc);
+//
+//   e.target.name.value = null;
+//   e.target.email.value = null;
+//   e.target.descript.value = null;
+//   e.target.loc.value = null;
 }
 
-form.addEventListener('submit', handleForm);
+form.addEventListener('submit', renderAsRow);

0e1d7a688f9f23f5a07ff5f71c082f173a5447eb
Iryna Maslova <maslovai@hotmail.com>
2017-03-13 20:35:13 -0700

    split js in two files, table rendering in a postboard page

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var clientArray = [];
-var table = document.getElementById('postBoard');
+// var table = document.getElementById('postBoard');
 function Client(name, email, descript, loc, time){
 
   this.name = name;
@@ -9,12 +9,6 @@ function Client(name, email, descript, loc, time){
   this.time = time;
 
 }
-
-// Creating a Post-Board Table
-//creating a table header function:
-var nameData;
-var jobNumber = 1;
-var users = [];
 
 // storing log in info
 
@@ -32,97 +26,8 @@ function userLs(event){
   // localStorage.getItem('userName', JSON.parse(userName));
   // localStorage.getItem('password', JSON.parse(password));
 }
-// var login = document.getElementById('userName');
-// var pass = document.getElementById('password');
 
-function renderHeader(){
-
-  var table = document.getElementById('postBoard');
-  var trElement = document.createElement('thead');
-  table.appendChild(trElement);
-
-  nameData = document.createElement('td');
-  nameData.textContent = " Job #";
-  trElement.appendChild(nameData);
-
-
-  nameData = document.createElement('td');
-  nameData.textContent = "Name";
-  trElement.appendChild(nameData);
-
-  nameData = document.createElement('td');
-  nameData.textContent = "E-mail";
-  trElement.appendChild(nameData);
-
-  nameData = document.createElement('td');
-  nameData.textContent = "Job Description";
-  trElement.appendChild(nameData);
-
-  nameData = document.createElement('td');
-  nameData.textContent = "Location";
-  trElement.appendChild(nameData);
-
-  nameData = document.createElement('td');
-  nameData.textContent = "Time Frame";
-  trElement.appendChild(nameData);
-
-  nameData = document.createElement('td');
-  nameData.textContent = "Accept job";
-  trElement.appendChild(nameData);
-
-};
-
-renderHeader();
-
-
-//rendering table rows function:
-  renderAsRow = function(Client){
-
-  trElement = document.createElement('tr');
-
-  nameData = document.createElement('td');
-  nameData.textContent = jobNumber;
-  trElement.appendChild(nameData);
-
-  jobNumber++;
-
-
-  nameData = document.createElement('td');
-  nameData.textContent = Client.name;
-  trElement.appendChild(nameData);
-
-
-  nameData = document.createElement('td');
-  nameData.textContent = Client.email;
-  trElement.appendChild(nameData);
-
-
-  nameData = document.createElement('td');
-  nameData.textContent = Client.descript;
-  trElement.appendChild(nameData);
-
-
-  nameData = document.createElement('td');
-  nameData.textContent = Client.loc;
-  trElement.appendChild(nameData);
-
-  nameData = document.createElement('td');
-  nameData.textContent = Client.time;
-  trElement.appendChild(nameData);
-
-  var checkbox = document.createElement("INPUT");
-  checkbox.type = "checkbox";
-  trElement.appendChild(checkbox);
-
-  table.appendChild(trElement);
-  checkbox.addEventListener("click", function() {
-     alert('Good Luck!!');
-   })
- };
-
-
-
-  function handleForm(event){
+function handleForm(event){
   event.preventDefault();
   console.log('handle form');
   var name = event.target.name.value;
@@ -131,14 +36,21 @@ renderHeader();
   var loc = event.target.loc.value;
   var time = event.target.time.value;
   var newClient = new Client(name, email, descript, loc, time);
+
   clientArray.push(newClient);
 
-  renderAsRow(newClient);
-  event.target.name.value = null;
-  event.target.email.value = null;
-  event.target.descript.value = null;
-  event.target.loc.value = null;
-  event.target.time.value = null;
+  //localStorage.setItem('lsClientArray', clientArray);
+  console.log(newClient);
+
+  localStorage.setItem('lsClient', JSON.stringify(newClient));
+
+  location.href='postboard.html'
+
+  // event.target.name.value = null;
+  // event.target.email.value = null;
+  // event.target.descript.value = null;
+  // event.target.loc.value = null;
+  // event.target.time.value = null;
 }
 
 var form  = document.getElementById('addForm');

840a0ebe3d64140d213e28053f2bc19f6da2ad6d
Iryna Maslova <maslovai@hotmail.com>
2017-03-15 17:35:54 -0700

    starting new branch, icons missing

---
+++ README.md
@@ -0,0 +1,5 @@
+# 201-final-project
+repo for final group project
+
+# Problem-Domain
+People in the community want everyday tasks done but don't have time to accomplish them. There are also people in the local community that want to earn extra cash or just have free time, we want to connect the people with needs and the people with services to offer.  
---
+++ appPostBoard.js
@@ -0,0 +1,117 @@
+var table = document.getElementById('postBoard');
+var nameData;
+var jobNumber = 1;
+var trElement;
+
+// assigning localstorage array to the new variable to use in rendering rows
+var clientArray = JSON.parse(localStorage.getItem('lsClientArray'));
+//console.log(clientArray);
+
+function renderHeader(){
+
+  trElement = document.createElement('thead');
+  table.appendChild(trElement);
+
+  nameData = document.createElement('td');
+  nameData.textContent = " Job #";
+  trElement.appendChild(nameData);
+
+
+  nameData = document.createElement('td');
+  nameData.textContent = "Name";
+  trElement.appendChild(nameData);
+
+  nameData = document.createElement('td');
+  nameData.textContent = "E-mail";
+  trElement.appendChild(nameData);
+
+  nameData = document.createElement('td');
+  nameData.textContent = "Job Description";
+  trElement.appendChild(nameData);
+
+  nameData = document.createElement('td');
+  nameData.textContent = "Location";
+  trElement.appendChild(nameData);
+
+  nameData = document.createElement('td');
+  nameData.textContent = "Time Frame";
+  trElement.appendChild(nameData);
+
+  nameData = document.createElement('td');
+  nameData.textContent = "Accept job";
+  trElement.appendChild(nameData);
+
+};
+
+renderHeader();
+
+//rendering table rows function:
+renderAsRow = function(client){
+  for (i = 0; i<clientArray.length; i++){
+
+    trElement = document.createElement('tr');
+
+    nameData = document.createElement('td');
+    nameData.textContent = jobNumber;
+    trElement.appendChild(nameData);
+
+    jobNumber++;
+
+    nameData = document.createElement('td');
+    nameData.textContent = client[i].name;
+    trElement.appendChild(nameData);
+
+
+    nameData = document.createElement('td');
+    nameData.textContent = client[i].email;
+    trElement.appendChild(nameData);
+
+
+    nameData = document.createElement('td');
+    nameData.textContent = client[i].descript;
+    trElement.appendChild(nameData);
+
+
+    nameData = document.createElement('td');
+    nameData.textContent = client[i].loc;
+    trElement.appendChild(nameData);
+
+    nameData = document.createElement('td');
+    nameData.textContent = client[i].time;
+    trElement.appendChild(nameData);
+
+    var checkbox = document.createElement('INPUT');
+    checkbox.type = "checkbox";
+    trElement.appendChild(checkbox);
+
+     // dimming checjbox on click
+    checkbox.addEventListener('click', function() {
+     if (this.checked){
+       this.parentElement.setAttribute('class', 'unavailable');
+     } else {
+       this.parentElement.setAttribute('class', 'available');
+     }
+   })
+
+    table.appendChild(trElement);
+
+  }
+}
+renderAsRow(clientArray);
+
+//redirecting to maps
+document.getElementById('goMaps').addEventListener('click', goToMaps);
+
+function goToMaps(){
+  location.href='job-location.html'
+}
+
+
+// document.getElementsByTagName('INPUT').addEventListener('click', function() {
+//   if (checkbox.checked){
+//     trElement.setAttribute('class', 'unavailable');
+//   } else {
+//     trElement.setAttribute('class', 'available');
+//   }
+// })
+console.log(clientArray);

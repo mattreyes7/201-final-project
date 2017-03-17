8da2b61d8b659f343f47ff78a5583926f1b0090d
Iryna Maslova <maslovai@hotmail.com>
2017-03-16 19:28:48 -0700

    showing cities on map dynamicaly

--- appPostBoard.js
+++ appPostBoard.js
@@ -5,12 +5,28 @@ var nameData;
 
 var jobNumber = 1;
 var trElement;
-
+var cityName;
+var cityMarker;
 // assigning localstorage array to the new variable to use in rendering rows
 
 var clientArray = JSON.parse(localStorage.getItem('lsClientArray'));
 //console.log(clientArray);
 
+var citiesCoordinates = [
+  {name:'seattle', lat:47.6062, lng:-122.3321},
+  {name:'redmond', lat:47.6740, lng:-122.1215},
+  {name:'bellevue', lat:47.6101, lng:-122.2015},
+  {name:'sammamish', lat:47.6163, lng:-122.0356},
+  {name:'kirkland', lat: 47.6769,  lng:-122.2060},
+  {name:'mercer island', lat: 7.5707, lng:-122.2221},
+  {name: 'bothel', lat: 7.7610, lng:-122.2056},
+  {name: 'new castle', lat: 47.5390, lng:-122.1557},
+  {name:'renton', lat: 47.4829, lng:-122.2171},
+  {name: 'issaquah', lat: 47.5301, lng: -122.0326},
+  {name: 'carnation', lat:47.6479, lng:-121.914}
+];
+var cityNamesArray = ['seattle', 'redmond', 'bellevue', 'sammamish', 'kirkland', 'mercer island',
+'bothel', 'new castle', 'renton', 'issaquah', 'carnation' ];
 function renderHeader(){
 
   trElement = document.createElement('thead');
@@ -56,7 +72,7 @@ function renderAsRow(client){
 
     trElement = document.createElement('tr');
 
-    trElement = document.createElement('tr');
+
     trElement.setAttribute('class', 'available');
     nameData = document.createElement('td');
     nameData.textContent = jobNumber;
@@ -82,6 +98,8 @@ function renderAsRow(client){
 
     nameData = document.createElement('td');
     nameData.textContent = client[i].loc;
+    cityName = client[i].loc.toLowerCase();
+    console.log(cityName);
     trElement.appendChild(nameData);
 
     nameData = document.createElement('td');
@@ -105,34 +123,34 @@ function renderAsRow(client){
     })
 
     table.appendChild(trElement);
-
+    locateJob(cityName);
   }
 }
 
 renderAsRow(clientArray);
 
-function initMap() {
+function locateJob(cityName){
+  var index =cityNamesArray.indexOf(cityName);
+  console.log(index);
+  if (index ===-1) {
+    cityMarker = 11;
+  } else {
+    cityMarker = index;
+  }
+  initMap();
+  return('');
+}
 
-  var citiesCoordinates = [
-    {name:'seattle', lat:47.6062, lng:-122.3321},
-    {name:'redmond', lat:47.6740, lng:-122.1215},
-    {name:'bellevue', lat:47.6101, lng:-122.2015},
-    {name:'sammamish', lat:47.6163, lng:-122.0356},
-    {name:'kirkland', lat: 47.6769,  lng:-122.2060},
-    {name:'mercer island', lat: 7.5707, lng:-122.2221},
-    {name: 'bothel', lat: 7.7610, lng:-122.2056},
-    {name: 'new castle', lat: 47.5390, lng:-122.1557},
-    {name:'renton', lat: 47.4829, lng:-122.2171},
-    {name: 'issaquah', lat: 47.5301, lng: -122.0326}
-  ];
+function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
     center: {lat:47.6062, lng:-122.3321}
   });
-  for (i=0; i<citiesCoordinates.length;i++){
-    var marker = new google.maps.Marker({
-      position: {lat: citiesCoordinates[i].lat, lng: citiesCoordinates[i].lng},
-      map: map
-    });
-  }
+//for (i=0; i<citiesCoordinates.length;i++){
+  var marker = new google.maps.Marker({
+    position: {lat: citiesCoordinates[cityMarker].lat, lng: citiesCoordinates[cityMarker].lng},
+    map: map
+  });
+//  }
 }
+  //position: {lat: citiesCoordinates[cityMarker].lat, lng: citiesCoordinates[cityMarker].lng},
